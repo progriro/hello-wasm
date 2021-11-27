5f051776c85b2649f0d758ef157cc733ca43db20
progriro <progriro@gmail.com>
2021-11-27 18:57:09 +0900

    [created] code by using wasm_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+// wasm_bindgen: JavaScript と Rust を bridge する library
+// prelude: 関連するクラスを良い感じにインポートする宣言
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
