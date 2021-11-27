ee41283089d615a3eb1a220138bcc884a812ee6c
progriro <progriro@gmail.com>
2021-11-27 19:21:12 +0900

    [add] comfirm's test via wasm

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,23 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+    pub fn confirm(s: &str) -> bool;
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+fn add(n1: i32, n2: i32) -> i32 {
+    return n1 + n2;
+}
+
+#[wasm_bindgen]
+pub fn judge(n1: i32, n2: i32) {
+    if confirm(&format!("{} + {} = {} is true?", n1, n2, add(n1, n2))) {
+        alert("Yeah!");
+    } else {
+        alert("Ooops!");
+    }
 }
